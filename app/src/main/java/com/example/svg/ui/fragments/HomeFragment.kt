702bcde74e36130922c5c84cf2a5ef0d320e4bf1
glitched-sudhanshu <68722462+glitched-sudhanshu@users.noreply.github.com>
2023-09-03 21:04:00 +0530

    navigation between fragments

--- app/src/main/java/com/example/svg/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/svg/ui/fragments/HomeFragment.kt
@@ -1,5 +1,10 @@
 package com.example.svg.ui.fragments
 
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.svg.R
 import com.example.svg.databinding.FragmentHomeBinding
 import com.example.svg.viewmodels.DogsViewModel
@@ -7,6 +12,17 @@ import com.example.svg.viewmodels.DogsViewModel
 class HomeFragment : BaseFragment<FragmentHomeBinding, DogsViewModel>() {
 
   override fun getFragmentView() = R.layout.fragment_home
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
-//  override fun getViewModel(): Class<DogsViewModel> = DogsViewModel::class.java
+    binding.btnGenerateDogs.setOnClickListener{
+      findNavController().navigate(R.id.action_homeFragment_to_generateDogsFragment)
+    }
+
+    binding.btnRecentlyGeneratedDogs.setOnClickListener {
+
+      findNavController().navigate(R.id.action_homeFragment_to_recentlyGeneratedDogsFragment)
+    }
+  }
+  //  override fun getViewModel(): Class<DogsViewModel> = DogsViewModel::class.java
 }
